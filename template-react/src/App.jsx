5591a2ccb497fd552a587f6c4c3c8ddb995c1573
ClaudiaLorenaN <claudia.2210.cn@gmail.com>
2024-08-13 20:52:22 -0500

    first commit exam1

---
+++ template-react/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Card from "./Componentes/Card"
+
+
+function App() {
+  //const [count, setCount] = useState(0)
+  return (
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+  );
+}
+
+
+export default App;
+
+ 
